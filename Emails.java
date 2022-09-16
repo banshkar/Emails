3db48a43c8b68e0dd9ce84e6176fc184f711e2b0
jiendra <jitendra@banshkar.com>
2022-09-16 20:26:03 +0530

    UC_1

---
+++ Emails.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Emails {
+    public  void validEmail(String email){
+        Pattern pattern =Pattern.compile("^[a-zA-Z]+?(@yahoo.com)+$");
+        Matcher check =pattern.matcher(email);
+        if(check.matches()){
+            System.out.println("Email valid ");
+        }
+        else {
+            System.out.println("Email not valid ");
+        }
+
+    }
+    public static void main(String[] args) {
+        Emails emails =new Emails();
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter Email Id");
+        String email =sc.nextLine();
+        emails.validEmail(email);
+    }
+}
